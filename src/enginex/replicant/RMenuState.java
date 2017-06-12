b63740883c1caf87d94eda128583f281d3691c13
synju <nuerotronic@gmail.com>
2017-06-12 18:14:41 +0200

    Added escape key to quit game in menuState

--- src/enginex/replicant/RMenuState.java
+++ src/enginex/replicant/RMenuState.java
@@ -1,13 +1,17 @@
 package enginex.replicant;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
-import enginex.core.EngineX;
 import enginex.core.State;
 
 public class RMenuState extends State {
-	public RMenuState(EngineX game) {
+	Replicants game;
+	
+	public RMenuState(Replicants game) {
 		super(game);
+		
+		this.game = game;
 	}
 	
 	public void update() {
@@ -16,5 +20,10 @@ public class RMenuState extends State {
 	
 	public void render(Graphics2D g) {
 		
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		// Exit Game...
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE) game.exit();
 	}
 }
