aa983037cf30d31c31f1434cd45554b266ccebd9
synju <nuerotronic@gmail.com>
2017-06-13 12:18:54 +0200

    Added Buttons and Sounds Functionality

---
+++ src/enginex/replicant/Button.java
@@ -0,0 +1,73 @@
+package enginex.replicant;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+import enginex.core.GameObject;
+
+@SuppressWarnings("serial")
+public class Button extends GameObject {
+	Replicants	game;
+	boolean			hover				= false;
+	Image				defaultImage;
+	Image				hoverImage;
+	String			soundPath;
+	Point				m;
+	boolean			soundPlayed	= false;
+	
+	public Button(Replicants game, int x, int y, int w, int h, String defaultImagePath, String hoverImagePath) {
+		super(game);
+		this.game = game;
+		
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		
+		defaultImage = new ImageIcon(defaultImagePath).getImage();
+		hoverImage = new ImageIcon(hoverImagePath).getImage();
+	}
+	
+	public Button(Replicants game, int x, int y, int w, int h, String defaultImagePath, String hoverImagePath, String soundPath) {
+		super(game);
+		this.game = game;
+		
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		
+		defaultImage = new ImageIcon(defaultImagePath).getImage();
+		hoverImage = new ImageIcon(hoverImagePath).getImage();
+		this.soundPath = soundPath;
+	}
+	
+	public void update() {
+		hover = false;
+		m = game.getMousePosition();
+		
+		if((m != null) && (m.x > this.x && m.x < this.x + this.w) && (m.y > this.y && m.y < this.y + this.h)) {
+			hover = true;
+			if(soundPlayed == false) {
+				new AePlayWave(soundPath).run();
+				soundPlayed = true;
+			}
+		}
+		else {
+			soundPlayed = false;
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		
+		if(!hover)
+			g.drawImage(defaultImage, (int)x, (int)y, null);
+		else
+			g.drawImage(hoverImage, (int)x, (int)y, null);
+	}
+}
--- src/enginex/replicant/RMenuState.java
+++ src/enginex/replicant/RMenuState.java
@@ -8,18 +8,22 @@ import enginex.core.State;
 public class RMenuState extends State {
 	Replicants game;
 	
+	Button powerButton;
+	
 	public RMenuState(Replicants game) {
 		super(game);
 		
 		this.game = game;
+		
+		powerButton = new Button(game, game.width-32-10,0+10,32,32, "res/replicants/powerOn.png","res/replicants/powerHover.png", "res/replicants/sfx/buttonHover.wav");
 	}
 	
 	public void update() {
-		
+		powerButton.update();
 	}
 	
 	public void render(Graphics2D g) {
-		
+		powerButton.render(g);
 	}
 	
 	public void keyPressed(KeyEvent e) {
